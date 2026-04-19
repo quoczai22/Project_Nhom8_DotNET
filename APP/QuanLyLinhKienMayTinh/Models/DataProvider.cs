3d44f5c74b1974b2774b9a0e07d785e536989486
Quan Nguyen <quan02077@gmail.com>
2026-04-19 22:10:39 +0700

    Đổi lại tên server

--- APP/QuanLyLinhKienMayTinh/Models/DataProvider.cs
+++ APP/QuanLyLinhKienMayTinh/Models/DataProvider.cs
@@ -25,7 +25,7 @@ namespace QuanLyLinhKienMayTinh.Models
         private DataProvider()
         {
            var options= new DbContextOptionsBuilder<QL_LinhKien_PC_Context>()
-                .UseSqlServer("Data Source=localhost;Initial Catalog=QL_LinhKien_PC_NET;Integrated Security=True;TrustServerCertificate=True",
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=QL_LinhKien_PC_NET;Integrated Security=True;TrustServerCertificate=True",
                 sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount:5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null))
                 .Options;
 
@@ -33,7 +33,7 @@ namespace QuanLyLinhKienMayTinh.Models
         }
         public void ChangeToQuanLyConnection()
         {
-            string connStr = "Data Source=localhost;Initial Catalog=QL_LinhKien_PC_NET;User Id=QuanLyLogin;Password=123;TrustServerCertificate=True;";
+            string connStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=QL_LinhKien_PC_NET;User Id=QuanLyLogin;Password=123;TrustServerCertificate=True;";
             var options = new DbContextOptionsBuilder<QL_LinhKien_PC_Context>()
                  .UseSqlServer(connStr, sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null))
                  .Options;
@@ -43,7 +43,7 @@ namespace QuanLyLinhKienMayTinh.Models
 
         public void ChangeToNhanVienConnection()
         {
-            string connStr = "Data Source=localhost;Initial Catalog=QL_LinhKien_PC_NET;User Id=NhanVienBanHangLogin;Password=123;TrustServerCertificate=True;";
+            string connStr = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=QL_LinhKien_PC_NET;User Id=NhanVienBanHangLogin;Password=123;TrustServerCertificate=True;";
             var options = new DbContextOptionsBuilder<QL_LinhKien_PC_Context>()
                  .UseSqlServer(connStr, sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null))
                  .Options;
